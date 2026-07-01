fdfce15970600faf79d39f93161ede2a609ebd2a
Shanvi <0805shanvi@gmail.com>
2026-07-01 19:31:35 +0530

    count even and odd digits in a number

---
+++ count_even_odd.py
@@ -0,0 +1,17 @@
+num = int(input("Enter a number: "))
+
+even_count = 0
+odd_count = 0
+
+while num > 0:
+    digit = num % 10
+
+    if digit % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1
+
+    num = num // 10
+
+print("Even digits:", even_count)
+print("Odd digits:", odd_count)
